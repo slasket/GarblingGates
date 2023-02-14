8933713c52427c6f88ddf2a3cf3463c7091bd131
Shelleshoj <Shelleshoj@users.noreply.github.com>
2023-02-14 13:42:08 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+#include <boost/lambda/lambda.hpp>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <cryptoTools/Common/Defines.h>
 
-//cool project
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    oc::u64 a = 0;
+    oc::u64 b = 1;
+    oc::u64 c = a + b;
+    std::cout << c << std::endl;
+
+
+    std::for_each(
+            in(std::cin), in(), std::cout << (_1 * 3) << " " );
+
+
     return 0;
 }
