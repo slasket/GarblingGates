5e6205d37712ec08dc3d0fc8af5aa6db5f209290
Simon <sniomrold@hotmail.com>
2023-02-15 13:28:49 +0100

    Alternative way of writing boost tests

--- tests/test_class.cpp
+++ tests/test_class.cpp
@@ -1,28 +1,28 @@
+#define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
 using namespace boost::unit_test;
 
-void free_test_function()
+
+BOOST_AUTO_TEST_SUITE( Testing_bit_position_function )
+
+    BOOST_AUTO_TEST_CASE( test_does_it_work )
+    {
+        BOOST_TEST( true );
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_CASE( test_outside_of_test_suite )
 {
-    BOOST_TEST( true /* test assertion */ );
+    BOOST_TEST( true );
 }
 
-void scam_test_function()
-{
-    BOOST_TEST( 1 == 0);
-}
+BOOST_AUTO_TEST_SUITE( Testing_ElGamalStuff )
 
-void not_scam_test_function()
-{
-    BOOST_TEST(false);
-}
+    BOOST_AUTO_TEST_CASE( test_extract_exponent )
+    {
+        BOOST_TEST( true );
+    }
 
-test_suite* init_unit_test_suite( int /*argc*/, char* /*argv*/[] )
-{
-    framework::master_test_suite().
-            add( BOOST_TEST_CASE( &free_test_function ) );
-    framework::master_test_suite().
-            add( BOOST_TEST_CASE_NAME( &scam_test_function, "1. brick function" ) );
-    framework::master_test_suite().
-            add( BOOST_TEST_CASE_NAME( &not_scam_test_function, "2. brick function"));
-    return 0;
-}
+BOOST_AUTO_TEST_SUITE_END()
