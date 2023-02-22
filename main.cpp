0d2d9002dd41b608b868369b1716313fa40d5bb7
Shelleshoj <Shelleshoj@users.noreply.github.com>
2023-02-22 18:02:45 +0100

    todo refactor

--- main.cpp
+++ main.cpp
@@ -101,24 +101,73 @@ string sha256(const string str){
     return ss.str();
 }
 
-string aes256(unsigned char *str){
-    unsigned char *indata = reinterpret_cast<unsigned char *>(sizeof(str));
-    unsigned char *outdata = reinterpret_cast<unsigned char *>(4*sizeof(str));;
-    unsigned char ckey[] =  "thiskeyisverybad";
-    unsigned char ivec[] = "dontusethisinput";
+/**
+ * Create a 256 bit key and IV using the supplied key_data. salt can be added for taste.
+ * Fills in the encryption and decryption ctx objects and returns 0 on success
+ **/
+int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt, EVP_CIPHER_CTX *e_ctx,
+             EVP_CIPHER_CTX *d_ctx)
+{
+    int i, nrounds = 5;
+    unsigned char key[32], iv[32];
 
-    //fread(indata,sizeof(char),fsize, ifp);//Read Entire File
-    int inlen=sizeof(str);
-    int outLen1=0;
-    int outLen2=0;
-    int outLen=4*sizeof(str);
-    indata = reinterpret_cast<unsigned char *>(str);
-    EVP_CIPHER_CTX *ctx;
-    EVP_EncryptInit(ctx,EVP_aes_256_ctr(),ckey,ivec);
-    EVP_EncryptUpdate(ctx,outdata,&outLen1,indata,inlen);
-    cout<< outLen1 << " "<< outLen2<< endl;
-    EVP_EncryptFinal(ctx,outdata + outLen1,&outLen2);
-    return (string) reinterpret_cast<const char *const>(outdata);
+    /*
+     * Gen key & IV for AES 256 CTR mode. A SHA1 digest is used to hash the supplied key material.
+     * nrounds is the number of times the we hash the material. More rounds are more secure but
+     * slower.
+     */
+    i = EVP_BytesToKey(EVP_aes_256_cbc(), EVP_sha1(), salt, key_data, key_data_len, nrounds, key, iv);
+    if (i != 32) {
+        printf("Key size is %d bits - should be 256 bits\n", i);
+        return -1;
+    }
+
+    EVP_CIPHER_CTX_init(e_ctx);
+    EVP_EncryptInit_ex(e_ctx, EVP_aes_256_cbc(), NULL, key, iv);
+    EVP_CIPHER_CTX_init(d_ctx);
+    EVP_DecryptInit_ex(d_ctx, EVP_aes_256_cbc(), NULL, key, iv);
+
+    return 0;
+}
+
+/*
+ * Encrypt *len bytes of data
+ * All data going in & out is considered binary (unsigned char[])
+ */
+unsigned char *aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len)
+{
+    /* max ciphertext len for a n bytes of plaintext is n + AES_BLOCK_SIZE -1 bytes */
+    int c_len = *len + AES_BLOCK_SIZE, f_len = 0;
+    unsigned char *ciphertext = static_cast<unsigned char *>(malloc(c_len));
+
+    /* allows reusing of 'e' for multiple encryption cycles */
+    EVP_EncryptInit_ex(e, NULL, NULL, NULL, NULL);
+
+    /* update ciphertext, c_len is filled with the length of ciphertext generated,
+      *len is the size of plaintext in bytes */
+    EVP_EncryptUpdate(e, ciphertext, &c_len, plaintext, *len);
+
+    /* update ciphertext with the final remaining bytes */
+    EVP_EncryptFinal_ex(e, ciphertext+c_len, &f_len);
+
+    *len = c_len + f_len;
+    return ciphertext;
+}
+/*
+ * Decrypt *len bytes of ciphertext
+ */
+unsigned char *aes_decrypt(EVP_CIPHER_CTX *e, unsigned char *ciphertext, int *len)
+{
+    /* plaintext will always be equal to or lesser than length of ciphertext*/
+    int p_len = *len, f_len = 0;
+    unsigned char *plaintext = static_cast<unsigned char *>(malloc(p_len));
+
+    EVP_DecryptInit_ex(e, NULL, NULL, NULL, NULL);
+    EVP_DecryptUpdate(e, plaintext, &p_len, ciphertext, *len);
+    EVP_DecryptFinal_ex(e, plaintext+p_len, &f_len);
+
+    *len = p_len + f_len;
+    return plaintext;
 }
 
 int main() {
@@ -127,9 +176,65 @@ int main() {
     cout << sha256("1234567890_2") << endl;
     cout << sha256("1234567890_3") << endl;
     cout << sha256("1234567890_4") << endl;
-    unsigned char xd[20] ="hello world";
-    cout << aes256(xd) << endl;
 
+    unsigned char xd[20] ="hello world";
+    /* "opaque" encryption, decryption ctx structures that libcrypto uses to record
+        status of enc/dec operations */
+    EVP_CIPHER_CTX* en = EVP_CIPHER_CTX_new();
+    EVP_CIPHER_CTX* de = EVP_CIPHER_CTX_new();
+
+    /* 8 bytes to salt the key_data during key generation. This is an example of
+       compiled in salt. We just read the bit pattern created by these two 4 byte
+       integers on the stack as 64 bits of contigous salt material -
+       ofcourse this only works if sizeof(int) >= 4 */
+    unsigned int salt[] = {12345, 54321};
+    unsigned char *key_data;
+    int key_data_len, i;
+    char *input[] = {"012345678901234567890123456789010123456789012345678901234567890101234567890123456789012345678901012345678901234567890123456789010123456789012345678901234567890101234567890123456789012345678901012345678901234567890123456789010123456789012345678901234567890101234567890123456789012345678901012345678901234567890123456789010123456789012345678901234567890101234567890123456789012345678901012345678901234567890123456789010123456789012345678901234567890101234567890123456789012345678901012345678901234567890123456789010123456789012345678901234567890101234567890123456789012345678901", "abcd", "this is a test", "this is a bigger test",
+                     "\nWho are you ?\nI am the 'Doctor'.\n'Doctor' who ?\nPrecisely!",
+                     NULL};
+
+    key_data = (unsigned char *)"01234567890123456789012345678901";
+    key_data_len = strlen((char *)key_data);
+
+    /* gen key and iv. init the cipher ctx object */
+    if (aes_init(key_data, key_data_len, (unsigned char *)&salt, en, de)) {
+        printf("Couldn't initialize AES cipher\n");
+        return -1;
+    }
+
+    /* encrypt and decrypt each input string and compare with the original */
+    for (i = 0; input[i]; i++) {
+        char *plaintext;
+        unsigned char *ciphertext;
+        int olen, len;
+
+        /* The enc/dec functions deal with binary data and not C strings. strlen() will
+           return length of the string without counting the '\0' string marker. We always
+           pass in the marker byte to the encrypt/decrypt functions so that after decryption
+           we end up with a legal C string */
+        olen = len = 256;
+        //olen = len = strlen(input[i])+1;
+
+        ciphertext = aes_encrypt(en, (unsigned char *)input[i], &len);
+        //print the ciphertext
+        for (int j = 0; j < len; j++) {
+            printf("%02x", ciphertext[j]);
+        }
+        printf("\n");
+        plaintext = (char *)aes_decrypt(de, ciphertext, &len);
+
+        if (strncmp(plaintext, input[i], olen))
+            printf("FAIL: enc/dec failed for \"%s\"\n", input[i]);
+        else
+            printf("OK: enc/dec ok for \"%s\"\n", plaintext);
+
+        free(ciphertext);
+        free(plaintext);
+    }
+
+    EVP_CIPHER_CTX_free(en);
+    EVP_CIPHER_CTX_free(de);
 
     return 0;
 }
